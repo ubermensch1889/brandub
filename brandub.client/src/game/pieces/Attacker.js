f17b961c137402dcdf81bf2ae08a0846caffe580
Erentsen <sangadzhiev1406@mail.ru>
2024-03-05 19:29:21 +0300

    add a page with 2 players local game including game logic

---
+++ brandub.client/src/game/pieces/Attacker.js
@@ -0,0 +1,7 @@
+﻿import Piece from './Piece.js';
+
+export default class Attacker extends Piece {
+    constructor(){
+        super("attacker",  "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg")
+    }
+}
---
+++ brandub.client/src/game/pieces/Defender.js
@@ -0,0 +1,7 @@
+﻿import Piece from './Piece.js';
+
+export default class Defender extends Piece {
+    constructor(){
+        super("defender", "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg")
+    }
+}
