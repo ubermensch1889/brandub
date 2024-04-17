574a5c2bc15e602c7301bd6c02259ebba2535950
Erentsen <sangadzhiev1406@mail.ru>
2024-04-17 12:11:40 +0300

    minor design fix

--- brandub.Server/obj/Debug/net6.0/brandub.Server.AssemblyInfo.cs
+++ brandub.Server/obj/Debug/net6.0/brandub.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("brandub.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d7f8d5daea1c6bb8bd9c714fedd07a2b2a4a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("brandub.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("brandub.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
